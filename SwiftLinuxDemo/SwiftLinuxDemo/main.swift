b10ff9afaba83c20dc0ae092811fb7d358b70553
wangqinghai <212032326@qq.com>
2016-08-10 23:41:48 +0800

    a

---
+++ SwiftLinuxDemo/SwiftLinuxDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftLinuxDemo
+//
+//  Created by CaiLianfeng on 16/8/10.
+//  Copyright © 2016年 WangQinghai. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
